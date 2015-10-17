695d8479402fd8dc4db8778fa9efb9708ebdd986
darrenZgreat <schlagerd@live.com>
2015-10-17 15:04:12 -0600

    Added files

---
+++ Vm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+typedef struct {
+	int opCode;
+	int parameter;
+} command;
+
+int main(int argc, char *argv[]) 
+{
+	
+}
